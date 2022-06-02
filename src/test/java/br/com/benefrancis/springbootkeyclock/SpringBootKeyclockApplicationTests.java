fd55bc3ad314bd804a821ca3c604f4380aaba9e2
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-06-02 02:47:48 -0300

    Primeiro commit

---
+++ src/test/java/br/com/benefrancis/springbootkeyclock/SpringBootKeyclockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.springbootkeyclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeyclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
